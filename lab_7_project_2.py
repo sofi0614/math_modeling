3d0aa837ea693ae92c1bdea5aeeeaa41b111bfb9
sofi0614 <144795684+sofi0614@users.noreply.github.com>
2024-04-08 18:42:31 +0000

    проект

---
+++ lab_7_project_2.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+fig, ax = plt.subplots() 
+
+anim_object1, = plt.plot([], [], '-', lw=2) 
+anim_object2, = plt.plot([], [], '-', lw=2)
+anim_object3, = plt.plot([], [], '-', lw=2)
+
+xdata1, ydata1 = [], [] 
+xdata2, ydata2 = [], [] 
+xdata3, ydata3 = [], [] 
+
+ax.set_xlim(0, 6*np.pi) 
+ax.set_ylim(-7, 7) 
+
+def update(frame):
+    xdata1.append(frame)
+    ydata1.append(np.sin(frame+np.pi))
+    anim_object1.set_data(xdata1, ydata1) 
+
+    xdata2.append(frame) 
+    ydata2.append(np.sin(frame)+5)
+    anim_object2.set_data(xdata2, ydata2) 
+
+    xdata3.append(frame) 
+    ydata3= np.array(ydata1)+np.array(ydata2)-9
+    anim_object3.set_data(xdata3, ydata3) 
+
+ani = FuncAnimation(fig,
+                    update, 
+                    frames=np.arange(0, 6*np.pi, 0.1),
+                    interval=100 
+                    )     
+
+
+ani.save('proect_2_task.gif')
